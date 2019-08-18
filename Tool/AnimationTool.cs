12c4cdb8e64d8f6fdede4930118e7d786d9f0d4f
Ocean <762923309@qq.com>
2019-08-18 23:51:13 +0800

    修改贝塞尔曲线

--- Tool/AnimationTool.cs
+++ Tool/AnimationTool.cs
@@ -5,17 +5,29 @@ using UnityEngine.Events;
 
 public class AnimationTool
 {
-    public static void PlayForward(Animation animation, string clipName)
+    public static void PlayForward(Animation animation, string clipName,UnityAction onComplete = null)
     {
         animation[clipName].speed = 1;
         animation[clipName].normalizedTime = 0;
         animation.Play();
+
+        TimeDelay.Delay(animation[clipName].length, () =>
+        {
+            if (onComplete != null)
+                onComplete();
+        });
     }
-    public static void PlayBack(Animation animation, string clipName)
+    public static void PlayBack(Animation animation, string clipName, UnityAction onComplete = null)
     {
         animation[clipName].speed = -1;
         animation[clipName].normalizedTime = 1;
         animation.Play();
+
+        TimeDelay.Delay(animation[clipName].length, () =>
+        {
+            if (onComplete != null)
+                onComplete();
+        });
     }
 
     public static void Reset(Animation animation, string clipName)
--- Tool/BezierCurveTool.cs
+++ Tool/BezierCurveTool.cs
@@ -12,44 +12,45 @@ namespace MyFrameworkPure
         public delegate Vector3 GetBezierPoint4(Vector3 start, Vector3 startControl, Vector3 endControl, Vector3 end,
             float t);
 
-        public static void DoBezierMove(this Transform transform, GetBezierPoint3 getBezierPoint, Vector3 start, Vector3 control, Vector3 end, float duration)
+        public static void DoBezier3Move(this Transform transform, Vector3 start, Vector3 control, Vector3 end,float duration, UnityAction onComplete = null)
         {
+            if (control.Equals(Vector3.negativeInfinity))
+            {
+                Vector3 vect = end - start;
+                control = start +  vect * 0.5f + Vector3.up * vect.magnitude * 0.5f;
+            }
+
             float t = 0;
             TimeCounter.CreateTimeCounter(duration, () =>
             {
                 t += Time.deltaTime/duration;
-                Vector3 point = getBezierPoint(start, control, end, t);
+                Vector3 point = Bezier3(start, control, end, t);
                 transform.position = point;
-            }, null); 
+            }, ()=>
+            {
+                if (onComplete != null)
+                    onComplete();
+            }); 
         }
 
-        public static void DoBezierMove(this Transform transform, GetBezierPoint4 getBezierPoint, Vector3 start,
+        public static void DoBezier4Move(this Transform transform, Vector3 start,
             Vector3 startControl, Vector3 endControl, Vector3 end, float duration)
         {
             float t = 0;
             TimeCounter.CreateTimeCounter(duration, () =>
             {
                 t += Time.deltaTime / duration;
-                Vector3 point = getBezierPoint(start, startControl,endControl, end, t);
+                Vector3 point = Bezier4(start, startControl,endControl, end, t);
                 transform.position = point;
             }, null);
         }
 
-        //public static  Vector2 Bezier3(Vector2 start,Vector2 control, Vector2 end ,float t)
-        //{
-        //    return (((1-t)*(1-t)) * start) + (2 * t* (1 - t) * control) + ((t* t) * end);
-        //}
 
         public static Vector3 Bezier3(Vector3 start, Vector3 control, Vector3 end, float t)
         {
             return (((1 - t) * (1 - t)) * start) + (2 * t * (1 - t) * control) + ((t * t) * end);
         }
 
-
-        //public static Vector3 Bezier4(Vector2 s, Vector2 st, Vector2 et, Vector2 e, float t)
-        //{
-        //    return (((-s + 3* (st-et) + e)* t + (3* (s+et) - 6* st))* t + 3* (st-s))* t + s;
-        //}
 
         public static Vector3 Bezier4(Vector3 s, Vector3 st, Vector3 et, Vector3 e, float t)
         {
