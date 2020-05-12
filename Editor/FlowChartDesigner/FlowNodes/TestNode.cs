e1ccc341ebad5d24fd94a36212578ac208ea6137
Ocean <762923309@qq.com>
2020-05-12 14:19:38 +0800

    #新增# 流程图编辑器 区域线条绘制

---
+++ Editor/FlowChartDesigner/FlowNodes/TestNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FlowChartDesigner;
+using UnityEngine;
+
+public class TestNode : FlowNode
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
