d2d183c3b347c1b95e96778ee99c820edee14d26
Ocean <762923309@qq.com>
2022-05-10 21:23:45 +0800

    增加debugtool

---
+++ Tool/DebugTool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyFrameworkPure
+{
+    public class DebugTool
+    {
+        public static void LogHierarchy(Transform root)
+        {
+            string result = string.Empty;
+            PrintChildren(root,string.Empty,ref result);
+            Debug.Log(result);
+        }
+
+        static void PrintChildren(Transform t, string indent,ref string result)
+        {
+            int childCount = t.childCount;
+            result = $"{result}\n{indent}{t.name}";
+
+            var moreIndent = indent + "\t";
+            for (int i = 0; i < childCount; ++i)
+            {
+                var child = t.GetChild(i);
+                PrintChildren(child, moreIndent,ref result);
+            }
+        }
+    }
+}
+
