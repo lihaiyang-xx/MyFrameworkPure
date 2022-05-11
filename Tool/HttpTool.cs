287839f4d4031b41f08e8e7c9ec4e34df114000f
Ocean <762923309@qq.com>
2022-05-11 15:06:38 +0800

    增加httptool 协程方法

--- Tool/HttpTool.cs
+++ Tool/HttpTool.cs
@@ -5,42 +5,12 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 public class HttpTool
 {
-
-    public static async Task<string> Get(string url)
-    {
-        using (UnityWebRequest request = UnityWebRequest.Get(url))
-        {
-            await request.SendWebRequest();
-            if (request.isNetworkError || request.isHttpError)
-            {
-                Debug.Log(request.error);
-            }
-            else
-            {
-                return request.downloadHandler.text;
-            }
-        }
-
-        return null;
-    }
-
-    public static string GetNative(string uri)
-    {
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-        using (Stream stream = response.GetResponseStream())
-        using (StreamReader reader = new StreamReader(stream))
-        {
-            return reader.ReadToEnd();
-        }
-    }
-
+#if AsyncAwaitUtil
     public static async Task<Texture> GetTexture(string url)
     {
         using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
@@ -58,6 +28,8 @@ public class HttpTool
 
         return null;
     }
+
+    
 
     public static async Task<string> Post(string url,string json)
     {
@@ -116,5 +88,120 @@ public class HttpTool
         }
 
         return null;
+    }
+
+
+    public static async Task<string> Get(string url)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            await request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                return request.downloadHandler.text;
+            }
+        }
+
+        return null;
+    }
+
+    
+#endif
+
+    public static string GetNative(string uri)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static IEnumerator Get(string url, UnityAction<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string text = request.downloadHandler.text;
+                callback?.Invoke(text);
+            }
+        }
+    }
+
+    public static IEnumerator GetTexture(string url, UnityAction<Texture2D> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                var texture = DownloadHandlerTexture.GetContent(request);
+                callback?.Invoke(texture);
+            }
+        }
+    }
+
+
+    public static IEnumerator Post(string url, string json, UnityAction<string> callback)
+    {
+        using (UnityWebRequest request = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST))
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = new UploadHandlerRaw(bytes);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string text = request.downloadHandler.text;
+                callback?.Invoke(text);
+            }
+        }
+    }
+
+    public static IEnumerator Post(string url, WWWForm form, UnityAction<string> callback, Dictionary<string, string> headers = null)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Post(url, form))
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    request.SetRequestHeader(header.Key, header.Value);
+                }
+            }
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string text = request.downloadHandler.text;
+                callback?.Invoke(text);
+            }
+        }
     }
 }
