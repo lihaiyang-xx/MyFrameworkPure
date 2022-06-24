b41bff5667b0bcd3c89f7ba5b9eed8412d9b5fb8
lihaiyang <762923309@qq.com>
2022-06-24 13:51:40 +0800

    增加BaseEventTriggerListener

---
+++ Tool/BaseEventTriggerListener.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class BaseEventTriggerListener : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IUpdateSelectedHandler, ISelectHandler
+{
+    public UnityAction<PointerEventData> onClick;
+    public UnityAction<PointerEventData> onDoubleClick;
+    public UnityAction<PointerEventData> onDown;
+    public UnityAction<PointerEventData> onEnter;
+    public UnityAction<PointerEventData> onExit;
+    public UnityAction<PointerEventData> onUp;
+    public UnityAction<BaseEventData> onSelect;
+    public UnityAction<BaseEventData> onUpdateSelect;
+    public UnityAction<PointerEventData.InputButton, Vector3> onGlobalMouseDown;
+    public UnityAction onLongPressed;
+    public UnityAction onPressed;
+
+    public bool IsPressed => pressed;
+
+    private bool pressed;
+    private float pressTimeCount;
+    private float lastClickTime = -1;
+
+    private const float DoubleClickThreshold = 0.5f;
+    private const float LongPressedTimeThreshold = 0.5f;
+
+    public static BaseEventTriggerListener Get(GameObject go)
+    {
+        BaseEventTriggerListener listener = go.GetComponent<BaseEventTriggerListener>();
+        if (listener == null) listener = go.AddComponent<BaseEventTriggerListener>();
+        return listener;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClick?.Invoke(eventData);
+
+        //issue:eventData.clickCount android平台无效,Unity bug?
+        //if (eventData.clickCount == 2)
+        //{
+        //    onDoubleClick?.Invoke();
+        //}
+
+        if (Time.timeSinceLevelLoad - lastClickTime < DoubleClickThreshold)
+        {
+            onDoubleClick?.Invoke(eventData);
+            lastClickTime = -1;
+        }
+        lastClickTime = Time.timeSinceLevelLoad;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        onDown?.Invoke(eventData);
+        pressed = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onEnter?.Invoke(eventData);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onExit?.Invoke(eventData);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        onUp?.Invoke(eventData);
+        pressed = false;
+    }
+    public void OnSelect(BaseEventData eventData)
+    {
+       onSelect?.Invoke(eventData);
+    }
+    
+    public void OnUpdateSelected(BaseEventData eventData)
+    {
+        onUpdateSelect?.Invoke(eventData);
+    }
+
+    void Update()
+    {
+        HandlePressed();
+        HandleGlobalMouseInput();
+    }
+
+    void HandlePressed()
+    {
+        if (pressed)
+        {
+            onPressed?.Invoke();
+            pressTimeCount += Time.deltaTime;
+            if (pressTimeCount > LongPressedTimeThreshold)
+                onLongPressed?.Invoke();
+        }
+        else
+        {
+            pressTimeCount = 0;
+        }
+    }
+
+    void HandleGlobalMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Left, Input.mousePosition);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Right, Input.mousePosition);
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Middle, Input.mousePosition);
+        }
+    }
+}
--- Tool/EventTriggerListener.cs
+++ Tool/EventTriggerListener.cs
@@ -4,128 +4,33 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class EventTriggerListener : UnityEngine.EventSystems.EventTrigger
+public class EventTriggerListener : BaseEventTriggerListener, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public delegate void VoidDelegate(GameObject go);
-    public VoidDelegate onClick;
-    public VoidDelegate onDown;
-    public VoidDelegate onEnter;
-    public VoidDelegate onExit;
-    public VoidDelegate onUp;
-    public VoidDelegate onDrag;
-    public VoidDelegate onSelect;
-    public VoidDelegate onDeselect;
-    public VoidDelegate onUpdateSelect;
-    public VoidDelegate onEndDrag;
-    public VoidDelegate onPressed;
-    public VoidDelegate onLongPressed;
-
-    public delegate void DataDelegate(GameObject go, PointerEventData e);
-    public DataDelegate onClick_Data;
-    public DataDelegate onDown_Data;
-    public DataDelegate onUp_Data;
-    public DataDelegate onDrag_Data;
-    public DataDelegate onBeginDrag_Data;
-
+    public UnityAction<PointerEventData> onBeginDrag;
+    public UnityAction<PointerEventData> onDrag;
+    public UnityAction<PointerEventData> onEndDrag;
     public UnityAction<PointerEventData.InputButton, Vector3> onGlobalMouseDown;
 
-    private bool pressed;
-    private float pressTimeCount;
-    private const float LongPressedTimeThreshold = 0.5f;
 
-    public bool IsPressed => pressed;
-
-    public static EventTriggerListener Get(GameObject go)
+    public new static EventTriggerListener Get(GameObject go)
     {
         EventTriggerListener listener = go.GetComponent<EventTriggerListener>();
         if (listener == null) listener = go.AddComponent<EventTriggerListener>();
         return listener;
     }
-    public override void OnPointerClick(PointerEventData eventData)
-    {
-        onClick?.Invoke(gameObject);
-        onClick_Data?.Invoke(gameObject, eventData);
-    }
-    public override void OnPointerDown(PointerEventData eventData)
-    {
-        onDown?.Invoke(gameObject);
-        onDown_Data?.Invoke(gameObject, eventData);
 
-        pressed = true;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        onBeginDrag?.Invoke(eventData);
     }
 
-    public override void OnPointerEnter(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData)
     {
-        onEnter?.Invoke(gameObject);
-    }
-    public override void OnPointerExit(PointerEventData eventData)
-    {
-        onExit?.Invoke(gameObject);
-    }
-    public override void OnPointerUp(PointerEventData eventData)
-    {
-        onUp?.Invoke(gameObject);
-        onUp_Data?.Invoke(gameObject, eventData);
-
-        pressed = false;
-    }
-    public override void OnSelect(BaseEventData eventData)
-    {
-        onSelect?.Invoke(gameObject);
+        onDrag?.Invoke(eventData);
     }
 
-    public override void OnDeselect(BaseEventData eventData)
+    public void OnEndDrag(PointerEventData eventData)
     {
-        onDeselect?.Invoke(gameObject);
-    }
-
-    public override void OnBeginDrag(PointerEventData eventData)
-    {
-        onBeginDrag_Data?.Invoke(gameObject,eventData);
-    }
-
-    public override void OnDrag(PointerEventData eventData)
-    {
-        onDrag?.Invoke(gameObject);
-        onDrag_Data?.Invoke(gameObject, eventData);
-    }
-
-    public override void OnEndDrag(PointerEventData eventData)
-    {
-        onEndDrag?.Invoke(gameObject);
-    }
-    
-    public override void OnUpdateSelected(BaseEventData eventData)
-    {
-        onUpdateSelect?.Invoke(gameObject);
-    }
-
-    void Update()
-    {
-        if (pressed)
-        {
-            onPressed?.Invoke(gameObject);
-            pressTimeCount += Time.deltaTime;
-            if(pressTimeCount > LongPressedTimeThreshold)
-                onLongPressed?.Invoke(gameObject);
-
-        }
-        else
-        {
-            pressTimeCount = 0;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Left, Input.mousePosition);
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Right, Input.mousePosition);
-        }
-        else if (Input.GetMouseButtonDown(2))
-        {
-            onGlobalMouseDown?.Invoke(PointerEventData.InputButton.Middle, Input.mousePosition);
-        }
+        onEndDrag?.Invoke(eventData);
     }
 }
