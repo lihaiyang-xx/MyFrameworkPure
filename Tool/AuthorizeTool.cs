525b5120bb291ef53762db7870e2014fef9275b1
lihaiyang <762923309@qq.com>
2022-07-08 16:23:02 +0800

    增加授权工具类

---
+++ Tool/AuthorizeTool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyFrameworkPure;
+using UnityEngine;
+
+public class AuthorizeTool : MonoBehaviour
+{
+    public static bool IsValid(string date)
+    {
+        DateTime dateTime = DateTime.Parse(date);
+        long timeStamp = (new DateTimeOffset(dateTime)).ToUnixTimeSeconds();
+        return TimeTool.GetTimeStamp() < timeStamp;
+    }
+}
